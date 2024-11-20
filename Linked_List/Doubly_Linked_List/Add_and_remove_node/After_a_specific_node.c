da86b8cd4c24c62486fc32fe06dcef1552399064
manu-paul20 <manupaul535@gmail.com>
2024-11-20 20:28:16 +0530

    Deleted some promgrams in doubly linked list

--- Linked_List/Doubly_Linked_List/Add_and_remove_node/After_a_specific_node.c
+++ Linked_List/Doubly_Linked_List/Add_and_remove_node/After_a_specific_node.c
@@ -6,7 +6,6 @@ struct node{
     struct node *next;
 };
 struct node *head = NULL;
-void traverse();
 void add_node(int data);
 void delete_node();
 int main(){
@@ -33,74 +32,4 @@ int main(){
     }
 }
 
-void add_node(int data){
-    int searchData;
-    struct node *new_node = (struct node*)malloc(sizeof(struct node));
-    new_node->data = data;
-    new_node->prev = NULL;
-    new_node->next = NULL;
-    if(head==NULL){
-        head=new_node;
-        printf("Node added\n");
-    }else{
-        printf("Enter data of the node you want to find : ");
-        scanf("%d",&searchData);
-        struct node *ptr = head;
-        while(ptr->next != NULL){
-            if(ptr->data==searchData){
-                break;
-            }else{
-                ptr = ptr->next;
-            }
-        }
-        if(ptr->data!=searchData){
-            printf("Node not found\n");
-        }else{
-            new_node->next = ptr->next;
-            ptr->next = new_node;
-            new_node->prev = ptr;
-            printf("Node added\n");
-        }
-    }
-}
-
-void delete_node(){
-     int searchData;
-     if(head==NULL){
-        printf("Linked list is empty\n");
-     }else if(head->next == NULL){
-        head=NULL;
-        printf("Linked list is empty now\n");
-     }else{
-        printf("Enter data of the node you want to find : ");
-        scanf("%d",&searchData);
-        struct node *ptr = head;
-        while(ptr->next != NULL){
-            if(ptr->data==searchData){
-                break;
-            }else{
-                ptr = ptr->next;
-            }
-        }
-        if(ptr->data!=searchData){
-            printf("Node not found\n");
-        }else{
-            struct node *temp = ptr->next;
-            ptr->next = ptr->next->next;
-            ptr->next->prev = ptr;
-            free(temp);
-            printf("Node deleted\n");
-        }
-     }
-
-}
-void traverse(){
-    struct node *ptr = head;
-    while(ptr->next!=NULL){
-        printf("%d\n",ptr->data);
-        ptr=ptr->next;
-    }
-    if(ptr->next==NULL){
-        printf("%d\n",ptr->data);
-    }
-}
+// All functions are deleted due to some issues .  They will be added soon
